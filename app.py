c16af7ab5bd6c3c6ba1b60f3b9849053a7cee541
rodolphodgo <rodolphogouveiageo@gmail.com>
2025-03-28 21:58:21 -0300

    UPDATE alteração na tela de login

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ import streamlit as st
 
 st.set_page_config(page_title="DataNExT", layout="centered")
 
-st.title("DataNExt")
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #C23B22; /* Cor de fundo */
+        }
+        .stApp {
+            background-color: #C23B22;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("DataNExT")
 
 empresa = st.text_input("Nome da Companhia", placeholder="Digite o nome da empresa", label_visibility="visible")
 
-logo = st.text_input("Logo URL", placeholder="https://example.com/logo.png", label_visibility="visible")
-
-#if right.button("Material button", icon=":material/mood:", use_container_width=True):
-#right.markdown("You clicked the Material button.")
+col1, col2 = st.columns([0.9, 0.1], vertical_alignment="bottom")
+with col1:
+    logo = st.text_input("Logo URL", placeholder="https://example.com/logo.png", label_visibility="visible")
+with col2:
+    upload_logo = st.button("", on_click=None, type="secondary", icon=":material/add_circle:", disabled=False, use_container_width=False)
 
 upload_csv = st.file_uploader("Upload CSV", type="csv", on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 
