0cdde555c77f86fa5c4ea1b0f5df79bad4ec91ab
juliacelly <cellyjulia@gmail.com>
2025-03-31 22:00:59 -0300

    Funcionalidade de Botão

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(page_title="DataNExT", layout="centered")
+main_page = st.Page("main_page.py", title="DataNExT", icon="🎈")
+page_2 = st.Page("page_02.py", title="Page 2", icon="❄️")
 
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: #C23B22; /* Cor de fundo */
-        }
-        .stApp {
-            background-color: #C23B22;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+pg = st.navigation([main_page, page_2])
 
-st.title("DataNExT")
-
-empresa = st.text_input("Nome da Companhia", placeholder="Digite o nome da empresa", label_visibility="visible")
-
-col1, col2 = st.columns([0.9, 0.1], vertical_alignment="bottom")
-with col1:
-    logo = st.text_input("Logo URL", placeholder="https://example.com/logo.png", label_visibility="visible")
-with col2:
-    upload_logo = st.button("", on_click=None, type="secondary", icon=":material/add_circle:", disabled=False, use_container_width=False)
-
-upload_csv = st.file_uploader("Upload CSV", type="csv", on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
-
-botao_analise = st.button("Gerar Análise", on_click=None, type="primary", disabled=True, use_container_width=True)
+pg.run()
---
+++ main_page.py
@@ -0,0 +1,82 @@
+import streamlit as st
+
+st.set_page_config(page_title="DataNExT", layout="centered")
+
+st.markdown(
+    """
+    <style>
+    div[data-testid="stFileUploader"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #C23B22; /* Cor de fundo */
+        }
+        .stApp {
+            background-color: #C23B22;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("DataNExT")
+
+empresa = st.text_input("Nome da Companhia", placeholder="Digite o nome da empresa", label_visibility="visible")
+
+col1, col2 = st.columns([0.9, 0.1], vertical_alignment="bottom")
+with col1:
+    logo = st.text_input("Logo URL", placeholder="https://example.com/logo.png", label_visibility="visible")
+with col2:
+    uploaded_logo = st.file_uploader("", type=["png", "jpg", 'jpeg'])
+    st.markdown(
+        
+        """
+        <style>
+        .upload-button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: white;
+            cursor: pointer;
+            border: 2px solid #C23B22;
+            box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+            transition: 0.3s;
+            position: relative;
+        }
+        .upload-button:hover {
+            background-color: #f5f5f5;
+        }
+        .hidden-upload {
+            opacity: 0;
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            cursor: pointer;
+        }
+        </style>
+        <label>
+            <input type="file" id="logo_upload" class="hidden-upload" accept=".png, .jpg, .jpeg" onchange="fileUpload()">
+            <div class="upload-button">+</div>
+        </label>
+        """,
+        unsafe_allow_html=True
+    )
+    
+upload_csv = st.file_uploader("Upload CSV", type="csv")
+
+botao_analise = st.button("Gerar Análise", type="primary", use_container_width=True)
+
+if upload_csv != None:
+    if botao_analise == True:
+        st.write("Carregando Dashboard")
